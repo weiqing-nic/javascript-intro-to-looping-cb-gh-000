46cd8d09a5aeb6d5bb3824e11d530ae68d2a0db2
weiqing lee <yoweiqing@gmail.com>
2018-05-29 12:21:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forloop(array){
   for(let i = 1; i <25; i++){
-  array.push('I am ${i} strange loop$(i === 0 ? '' : 's' ).')
+  array.push("I am ${i} strange loop$(i === 0 ? '' : 's' ).")
   }
   return array;
 }
